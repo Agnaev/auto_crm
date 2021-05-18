3ecce151ca122030b95f292083b4be930ab03413
Agnaev <Black8192water@gmail.com>
2021-05-18 21:01:59 +0300

    add cors

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,12 +1,14 @@
 import bodyParser from "body-parser"
+import cors from 'cors'
+
 import AuthRouter from './AuthRouter.js'
 import testRouter from './testAuthProtection.js'
-
 import { useAuth } from "./middlewares/useAuth.js"
 
 export function setRoutes (app) {
 	app.use(bodyParser.json())
 	app.use(bodyParser.urlencoded({ extended: false }))
+	app.use(cors())
 	app.use('/auth', AuthRouter)
 	app.use('/test', useAuth, testRouter)
 }
