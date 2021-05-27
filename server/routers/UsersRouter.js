b56400c93d44eef77661498f63dd6ce10ea2cba8
Agnaev <Black8192water@gmail.com>
2021-05-27 15:44:07 +0300

    get mechanics list api

--- server/routers/UsersRouter.js
+++ server/routers/UsersRouter.js
@@ -2,15 +2,17 @@ import express from 'express'
 import {
 	getUsersList,
 	updateUser,
-	deleteUser
+	deleteUser,
+	getMechanicsList
 } from '../controllers/UsersController.js'
 import { useAuth } from './middlewares/useAuth.js'
-import {checkUserInRole, useRoles} from './middlewares/useRoles.js'
-import {ROLES} from '../helpers/ROLES.js'
+import { checkUserInRole, useRoles } from './middlewares/useRoles.js'
+import { ROLES } from '../helpers/ROLES.js'
 
 const router = express.Router()
 router.use(useAuth)
 router.use(useRoles)
+router.get('/mechanics', getMechanicsList)
 router.use(checkUserInRole(ROLES.admin))
 router.get('/', getUsersList)
 router.put('/', updateUser)
