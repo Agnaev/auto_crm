f0b5d4a743fd93aaee98c8702af3c1788b7e4c2d
Agnaev <Black8192water@gmail.com>
2021-06-01 00:13:52 +0300

    localize element plus

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
+import ElementPlus, { locale } from 'element-plus'
+import lang from 'element-plus/lib/locale/lang/ru'
 
 import App from './App.vue'
 import router from './router'
@@ -7,6 +8,8 @@ import store from './store'
 
 import 'element-plus/lib/theme-chalk/index.css'
 import './assets/styles/styles.scss'
+
+locale(lang)
 
 createApp(App)
   .use(store)
